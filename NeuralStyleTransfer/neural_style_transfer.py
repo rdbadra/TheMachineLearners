7aeb45021e64fe12153064759d9747ec848b8673
roberto <rdbadra@gmail.com>
2020-08-10 14:17:00 +0200

    finished neural style transfer implementation

--- NeuralStyleTransfer/neural_style_transfer.py
+++ NeuralStyleTransfer/neural_style_transfer.py
@@ -3,15 +3,15 @@ import torchvision.transforms as transforms
 import torch
 import torchvision.models as models
 import torch.nn.functional as F
+from torch import optim
 from torchvision.transforms.functional import to_pil_image
 import matplotlib.pyplot as plt
 
-content = "data/content.jpeg"
-style = "data/style.jpg"
-content_img = Image.open(content)
-style_img = Image.open(style)
 
-h, w = 256, 384
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using: {device}")
+
+h, w = 256, 256
 mean_rgb = (0.485, 0.456, 0.406)
 std_rgb = (0.229, 0.224, 0.225)
 
@@ -20,16 +20,16 @@ transformer = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean_rgb, std_rgb)])
 
+content = "data/content.jpeg"
+style = "data/style.jpg"
+
+content_img = Image.open(content)
+style_img = Image.open(style)
+
 content_tensor = transformer(content_img)
 style_tensor = transformer(style_img)
 
-print(content_tensor.shape)
-print(style_tensor.shape)
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model_vgg = models.vgg19(pretrained=True).features.to(device).eval()
-
-print(model_vgg)
 
 for param in model_vgg.parameters():
     param.requires_grad = False
@@ -42,28 +42,29 @@ def get_features(x, model, layers):
             features[layers[str(name)]] = x
     return features
 
-def gram_matrix(x):
-    n, c, h, w = x.size()
-    x = x.view(n*c, h*w)
-    gram = torch.mm(x, x.t())
-    return gram
+def gram_matrix(input):
+    n, c, h, w = input.size()
+    features = input.view(n * c, h * w)
+    G = torch.mm(features, features.t())
+    return G
 
-def get_content_loss(pred_features, target_features, layer):
-    target = target_features[layer]
-    pred = pred_features[layer]
-    loss = F.mse_loss(pred, target)
+def get_content_loss(input, target):
+    loss = F.mse_loss(input, target)
     return loss
 
-def get_style_loss(pred_features, target_features, style_layers_dict):
+def get_style_loss(pred_features, target_features, feature_layers, content_layer):
     loss = 0
-    for layer in style_layers_dict:
-        pred_fea = pred_features[layer]
-        pred_gram = gram_matrix(pred_fea)
-        n, c, h, w = pred_fea.shape
-        target_gram = gram_matrix(target_features[layer])
-        layer_loss = style_layers_dict[layer] * F.mse_loss(pred_gram, target_gram)
-        loss += layer_loss / (n * c * h * w)
+    for i, layer in feature_layers.items():
+        if(layer is not content_layer):
+            pred_fea = pred_features[layer]
+            pred_gram = gram_matrix(pred_fea)
+            n, c, h, w = pred_fea.shape
+            target_gram = gram_matrix(target_features[layer])
+            layer_loss = F.mse_loss(pred_gram, target_gram)
+            loss += layer_loss / (n * c * h * w)
     return loss
+
+content_layer = 'conv4_2'
 
 feature_layers = {
     '0': 'conv1_1',
@@ -82,27 +83,17 @@ style_features = get_features(sty_tensor, model_vgg, feature_layers)
 
 input_tensor = con_tensor.clone().requires_grad_(True)
 
-from torch import optim
-
 optimizer = optim.Adam([input_tensor], lr=0.01)
 
 num_epochs = 301
 content_weight = 1e1
 style_weight = 1e4
-content_layer = 'conv5_1'
-style_layers_dict = {
-    'conv1_1': 0.75,
-    'conv2_1': 0.5,
-    'conv3_1': 0.25,
-    'conv4_1': 0.25,
-    'conv5_1': 0.25
-}
 
 for epoch in range(num_epochs + 1):
     optimizer.zero_grad()
     input_features = get_features(input_tensor, model_vgg, feature_layers)
-    content_loss = get_content_loss(input_features, content_features, content_layer)
-    style_loss = get_style_loss(input_features, style_features, style_layers_dict)
+    content_loss = get_content_loss(input_features[content_layer], content_features[content_layer])
+    style_loss = get_style_loss(input_features, style_features, feature_layers, content_layer)
     neural_loss = content_weight * content_loss + style_weight * style_loss
     neural_loss.backward()
     optimizer.step()
@@ -121,3 +112,4 @@ def imgtensor2pil(img_tensor):
 
 plt.imshow(imgtensor2pil(input_tensor[0].cpu()))
 plt.show()
+
